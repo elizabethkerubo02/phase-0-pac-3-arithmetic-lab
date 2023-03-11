7e38a3aa498c6a45fecb1657d1357263ccb7c008
elizabethkerubo02 <keruboelizabeth02@gmail.com>
2023-03-11 20:56:31 +0300

    completed the assignment

---
+++ index.js
@@ -0,0 +1,47 @@
+ function add(num1,num2){
+    return num1 + num2;
+}
+add(2,3);
+
+function subtract(e,f){
+    return e-f;
+}
+subtract(90,260);
+
+//multiply
+function multiply(a,b){
+    return a * b;
+}
+multiply(4,8);
+
+//divide
+function divide (c,d){
+    return c / d;
+}
+divide(16,4);
+
+
+// var n = 500;
+// function increment(){
+  
+//   return n++;
+// }
+function increment(n){
+    return n+=1;
+}
+//decrement function
+
+function decrement(n){
+    return n-=1;
+}
+function makeInt(n){
+    return parseFloat(n)
+}
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
+    
+  
+  
+
